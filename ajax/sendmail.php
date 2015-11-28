173d2872f1c50c0fe1564d32739ea1aff10bad83
Paycho <matt@giml.in>
2015-11-27 23:16:01 -0700

    Add sendmail logic
    PHP + JS to support sending feedback from the site using mailgun

---
+++ ajax/sendmail.php
@@ -0,0 +1,62 @@
+if(empty($_POST) || !isset($_POST)) {
+  ajaxResponse('error', 'Post cannot be empty.');
+} else {
+  $postData = $_POST;
+  $dataString = implode($postData,",");
+  $mailgun = sendMailgun($postData);
+  if($mailgun) {
+    ajaxResponse('success', 'Great success.', $postData, $mailgun);
+  } else {
+    ajaxResponse('error', 'Mailgun did not connect properly.', $postData, $mailgun);
+  }
+}
+
+function ajaxResponse($status, $message, $data = NULL, $mg = NULL) {
+  $response = array (
+    'status' => $status,
+    'message' => $message,
+    'data' => $data,
+    'mailgun' => $mg
+    );
+  $output = json_encode($response);
+  exit($output);
+}
+
+function sendMailgun($data) {
+  $api_key = 'key-2nbudgbi9nnzicx4-tefg2eco6exm9y2';
+  $api_domain = 'yanaduday.com';
+  $send_to = 'contact@yanaduday.com';
+
+  $name = $data['name'];
+  $email = $data['email'];
+  $content = $data['message'];
+  $messageBody = "Contact: $name ($email)\n\nMessage: $content";
+
+  $config = array();
+  $config['api_key'] = $api_key;
+  $config['api_url'] = 'https://api.mailgun.net/v2/'.$api_domain.'/messages';
+
+  $message = array();
+  $message['from'] = $email;
+  $message['to'] = $send_to;
+  $message['h:Reply-To'] = $email;
+  $message['subject'] = "Contact from yanaduday.com - " . date(DATE_RFC2822);
+  $message['text'] = $messageBody;
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $config['api_url']);
+  curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  curl_setopt($curl, CURLOPT_USERPWD, "api:{$config['api_key']}");
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($curl, CURLOPT_POST, true); 
+  curl_setopt($curl, CURLOPT_POSTFIELDS,$message);
+
+  $result = curl_exec($curl);
+
+  curl_close($curl);
+  return $result;
+ 
+}
